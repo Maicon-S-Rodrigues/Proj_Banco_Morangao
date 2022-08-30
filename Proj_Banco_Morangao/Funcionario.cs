8b97f2c0148df9d001d97defad3a79128560760c
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-08-30 10:19:49 -0300

    Primeira Construção da Classe "Funcionario" com suas propiedades e esboço para seus métodos

---
+++ Proj_Banco_Morangao/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Banco_Morangao
+{
+    internal class Funcionario
+    {
+        public String Nome { get; set; }
+        public int Matricula { get; set; }
+        public bool Gerente { get; set; }
+        public Agencia Agencia { get; set; }
+
+        public Funcionario (string nome, int matricula, bool gerente, Agencia agencia)
+        {
+            this.Nome = nome;
+            this.Matricula = matricula;
+            this.Gerente = gerente;
+            this.Agencia = agencia;
+        }
+
+        public void VerificarTipoDeConta ()
+        {
+
+        }
+        public void AprovarCadastroCliente()
+        {
+
+        }
+    }
+}
