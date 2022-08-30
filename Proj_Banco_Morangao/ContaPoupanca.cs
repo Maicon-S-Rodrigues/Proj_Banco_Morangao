2a268d6f120c5fe14b2c90ae48456fc374a00551
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-08-30 13:44:18 -0300

    Criada Conta Poupança ainda em brancco, apenas esboçada para começar a lógica pelo seu funcionamento

---
+++ Proj_Banco_Morangao/ContaPoupanca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Banco_Morangao
+{
+    internal class ContaPoupanca : ContaCorrente
+    {
+        public double SaldoContaPoupanca { get; set; }
+
+
+        public ContaPoupanca(int numeroDaConta, Cliente donoDaConta, Agencia agenciaDoCliente,
+            bool cartaoDeCredito, int numeroDoCartao, double saldoEmConta, double saldoChequeEspecial)
+            : base(numeroDaConta, donoDaConta, agenciaDoCliente, cartaoDeCredito, numeroDoCartao, saldoEmConta, saldoChequeEspecial)
+        {
+            this.SaldoContaPoupanca = 0;
+        }
+
+        public void AdicionarRendimento() ///calculo de porcentagem de rendimento mensal na poupança 
+        {
+
+        }
+    }
+}
